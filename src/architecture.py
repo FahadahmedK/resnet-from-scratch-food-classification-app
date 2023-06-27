4dc007e66b650b84dfb6669c6d0c5ded7d1c4f0c
Fahad Ahmed <fahad.ahmed95@live.com>
2023-06-27 18:17:15 +0000

    rectifying errors

--- src/architecture.py
+++ src/architecture.py
@@ -1,96 +1,94 @@
-import torch
 from torch import nn
 from torch.nn import functional as F
 
 class ResidualBlock(nn.Module):
-    
-    def __init__(self, outplanes, kernel_size=3, stride=1, padding=1, use_1x1conv=False):
+    def __init__(
+        self, outplanes, kernel_size=3, stride=1, padding=1, use_1x1conv=False
+    ):
         super().__init__()
-        
+
         self.conv1 = nn.LazyConv2d(
             out_channels=outplanes,
             kernel_size=kernel_size,
             padding=padding,
-            stride=stride
+            stride=stride,
         )
-        
+        self.bn1 = nn.LazyBatchNorm2d()
         self.conv2 = nn.LazyConv2d(
             out_channels=outplanes,
             kernel_size=kernel_size,
             padding=padding,
-            stride=stride
+            stride=stride,
         )
         if use_1x1conv:
             self.conv3 = nn.LazyConv2d(
-                out_channels=outplanes,
-                kernel_size=1,
-                stride=stride
+                out_channels=outplanes, kernel_size=1, stride=stride
             )
         else:
             self.conv3 = None
-        
-        self.bn1 = nn.BatchNorm2d()
-        self.bn2 = nn.BatchNorm2d()
-        
+
+        self.bn2 = nn.LazyBatchNorm2d()
+
     def forward(self, X):
         Y = F.relu(self.bn1(self.conv1(X)))
         Y = self.bn2(self.conv2(Y))
+        print(X.shape)
+        print(Y.shape)
         if self.conv3:
             X = self.conv3(X)
         Y += X
         return F.relu(Y)
-    
-        
+
+
 class ResNet(nn.Module):
-    
     def __init__(self, arch_config, input_channels=3, num_classes=2):
-        
         super().__init__()
-        
-        self.net = nn.Sequential()
-        self.net.add_module(nn.Sequential(
-            nn.Conv2d(
-                in_channels=input_channels,
-                out_channels=64,
-                kernel_size=7,
-                stride=2,
-                padding=3                
-            ),
-            nn.BatchNorm2d(),
-            nn.ReLU(),
-            nn.MaxPool2d(
-                kernel_size=3,
-                stride=2,
-                padding=1)
+
+        self.net =  nn.Sequential(
+                nn.Sequential(nn.Conv2d(
+                    in_channels=input_channels,
+                    out_channels=64,
+                    kernel_size=7,
+                    stride=2,
+                    padding=3,
+                ),
+                nn.BatchNorm2d(num_features=64),
+                nn.ReLU(),
+                nn.MaxPool2d(kernel_size=3, stride=2, padding=1),
             )
         )
-        
-        def SuperBlock(num_res_blocks, outplanes, first_block=False):     
+
+        def SuperBlock(num_res_blocks, outplanes, first_block=False):
             blk = []
-            for i in range(num_res_blocks):
-                if i == 0 and not first_block:
-                    blk.append(ResidualBlock(
-                        outplanes=outplanes,
-                        use_1x1conv=True,
-                        stride=2
-                    ))
+            for j in range(num_res_blocks):
+                if j == 0 and not first_block:
+                    blk.append(
+                        ResidualBlock(outplanes=outplanes, use_1x1conv=True, stride=2)
+                    )
                 else:
-                    blk.append(ResidualBlock(
-                        outplanes=outplanes
-                    ))
-                
-            return blk
-        
+                    blk.append(ResidualBlock(outplanes=outplanes))
+
+            return nn.Sequential(*blk)
+
         for i, super_block_config in enumerate(arch_config):
-            self.net.add_module(f'super_block {i+2}', self.SuperBlock(*super_block_config, first_block=(i==0)))
-        
-        self.net.add_module('last', nn.Sequential(
-            nn.AdaptiveAvgPool2d((1, 1)), 
-            nn.Flatten(),
-            nn.LazyLinear(num_classes)
-        )) 
-            
-        
+            self.net.add_module(
+                f"super_block {i+2}",
+                SuperBlock(*super_block_config, first_block=(i == 0)),
+            )
+
+        self.net.add_module(
+            "last",
+            nn.Sequential(
+                nn.AdaptiveAvgPool2d((1, 1)), nn.Flatten(), nn.LazyLinear(num_classes)
+            ),
+        )
+
+
 class ResNet18(ResNet):
-    def __init__(self, num_classes=2):
-        super().__init__(((2, 64), (2, 128), (2, 256), (2, 512)), num_classes=num_classes)
+    def __init__(self, input_channels=3, num_classes=2):
+        super().__init__(
+            ((2, 64), (2, 128), (2, 256), (2, 512)), input_channels=input_channels, num_classes=num_classes
+        )
+    def forward(self, X):
+        return self.net(X)
+    
--- src/preprocess.py
+++ src/preprocess.py
@@ -1,10 +1,10 @@
 import os
 import numpy as np
 import matplotlib.pyplot as plt
-from torchvision import datasets, transforms  
-        
-def transform(base_dir):
+from torchvision import datasets, transforms
 
+
+def transform(base_dir):
     transform_train = transforms.Compose(
         [
             transforms.RandomResizedCrop(224),
@@ -12,23 +12,27 @@ def transform(base_dir):
             transforms.RandomHorizontalFlip(p=0.5),
             transforms.RandomVerticalFlip(p=0.5),
             transforms.ToTensor(),
-            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-            ])
-    
-    transform_test = transforms.Compose([
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
+        ]
+    )
+
+    transform_test = transforms.Compose(
+        [
             transforms.Resize(256),
             transforms.CenterCrop(224),
             transforms.ToTensor(),
-            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-        ])
-        
-    
-    
-    return datasets.ImageFolder(root=os.path.join(base_dir, 'train'), transform=transform_train), datasets.ImageFolder(root=os.path.join(base_dir, 'test'), transform=transform_test) 
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
+        ]
+    )
 
-    
+    return datasets.ImageFolder(
+        root=os.path.join(base_dir, "train"), transform=transform_train
+    ), datasets.ImageFolder(
+        root=os.path.join(base_dir, "test"), transform=transform_test
+    )
+
+
 def imgshow(image_tensor, title=None):
-    
     image = image_tensor.numpy().permute((1, 2, 0))
     mean = np.array([0.485, 0.456, 0.406])
     std = np.array([0.229, 0.224, 0.225])
