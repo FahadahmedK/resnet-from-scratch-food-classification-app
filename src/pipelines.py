272ccf067cfaad075fa6762b44d6f58b681701ed
Fahad Ahmed <fahad.ahmed95@live.com>
2023-07-13 07:46:34 +0000

    integrating changes

--- src/pipelines.py
+++ src/pipelines.py
@@ -1,34 +1,71 @@
+import kfp
+
+# For creating the pipeline
 from kfp import dsl
-from kfp.dsl import component, Artifact, Output
-from preprocess import preprocess_images
+
+# For building components
+from kfp.dsl import component
+
+# Type annotations for the component artifacts
+from kfp.dsl import (
+    Input,
+    Output,
+    Artifact,
+    Dataset,
+    Model,
+    Metrics,
+    InputPath,
+    OutputPath,
+)
+#from preprocess import preprocess_images
+
+
+#@component(
+#    packages_to_install=["torchvision", "Pillow", "click", "torch", "numpy"],
+#    output_component_file="preprocessing_component.yaml",
+#)
+#def preprocessing_op(
+#    data_dir: InputPath(str),
+#    output_dir: InputPath(str),
+#    processed_data_artifact: Output[Artifact]
+#):
+ #   import os
+ #   import numpy as np
+ #   from torchvision import transforms
+ #   from torch.utils.data import DataLoader, Dataset
+ #   from torchvision.utils import save_image
+ #   preprocess_images(data_dir, output_dir, batch_size=32)
+  #  return output_dir
+
+#@dsl.pipeline(
+#    name="ML Pipeline",
+#    description="Pipeline for image classification"
+#)
+#def ml_pipeline(
+#    data_dir: str,
+#    output_dir: str,
+#):
+#    preprocessing_task = preprocessing_op(data_dir=data_dir, output_dir=output_dir)
+
 
 @component(
-    packages_to_install=["torchvision", "Pillow", "click", "torch", "numpy"],
-    output_component_file="preprocessing_component.yaml",
+    packages_to_install=["pandas", "openpyxl"],
 )
-def preprocessing_op(
-    data_dir: str,
-    output_dir: str,
-    processed_data_artifact: Output[Artifact]
-):
-    import os
-    import numpy as np
-    from torchvision import transforms
-    from torch.utils.data import DataLoader, Dataset
-    from torchvision.utils import save_image
-    preprocess_images(data_dir, output_dir, batch_size=32)
-    return output_dir
+def download_data(url:str, output_csv:Output[Dataset]):
+    import pandas as pd
+
+    # Use pandas excel reader
+    df = pd.read_excel(url)
+    df = df.sample(frac=1).reset_index(drop=True)
+    df.to_csv(output_csv.path, index=False)
+
 
 @dsl.pipeline(
     name="ML Pipeline",
     description="Pipeline for image classification"
 )
-def ml_pipeline(
-    data_dir: str,
-    output_dir: str,
-):
-    preprocessing_task = preprocessing_op(data_dir=data_dir, output_dir=output_dir)
-
+def ml_pipeline(url: str):
+    download_task = download_data(url=url)
 
 if __name__ == "__main__":
     import kfp.compiler as compiler
